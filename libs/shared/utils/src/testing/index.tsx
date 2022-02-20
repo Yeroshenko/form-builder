1ca1e37b467a5ed004cfc0607bf342ea1c6220d5
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2022-02-20 18:56:17 +0200

    :sparkles: feat(utils): create testing-utils

---
+++ libs/shared/utils/src/testing/index.tsx
@@ -0,0 +1,11 @@
+import { FC, ReactElement } from 'react'
+import { render, RenderOptions } from '@testing-library/react'
+import { ThemeProvider } from '@ui-kit'
+
+const AllTheProviders: FC = ({ children }) => <ThemeProvider>{children}</ThemeProvider>
+
+const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
+  render(ui, { wrapper: AllTheProviders, ...options })
+
+export * from '@testing-library/react'
+export { customRender as render }
